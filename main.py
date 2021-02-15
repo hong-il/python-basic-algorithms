c85024bb13a88a44a0b473b6c3d30cf5daae31e7
KIM HONGIL <clroyan01@gmail.com>
2021-02-16 00:15:40 +0900

    Two dimension list

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# List
 a = [i for i in range(20) if i % 2 == 1]
 # result : [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 print(a)
@@ -7,3 +8,9 @@ for i in range(20):
     if i % 2 == 1:
         a.append(i)
 print(a)
+# Two dimension list
+n = 4
+m = 3
+array = [[0] * m for _ in range(n)]
+# result : [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+print(array)

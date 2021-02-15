acc35c75514eb107bcf1d4401d98f3805a4d228d
KIM HONGIL <clroyan01@gmail.com>
2021-02-15 23:42:17 +0900

    Make integer list simple

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 a = [i for i in range(20) if i % 2 == 1]
 # result : [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 print(a)
+# same result
+a = []
+for i in range(20):
+    if i % 2 == 1:
+        a.append(i)
+print(a)

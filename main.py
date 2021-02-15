352a10659a3265d196486a2c100cd3e376a1af7b
KIM HONGIL <clroyan01@gmail.com>
2021-02-16 00:23:36 +0900

    List's functions : append, sort, reverse, insert, count, remove

--- main.py
+++ main.py
@@ -14,3 +14,16 @@ m = 3
 array = [[0] * m for _ in range(n)]
 # result : [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
 print(array)
+# List's functions
+# result : [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+a.append(21)
+# result : [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+a.sort()
+# result : [21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1]
+a.sort(reverse=1)
+# result : [21, 21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1]
+a.insert(1, 21)
+# result : 2
+a.count(21)
+# result : [21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1]
+a.remove(21)

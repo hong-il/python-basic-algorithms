53c71ba818571fb3248d2d8123b95973bd5754c8
KIM HONGIL <clroyan01@gmail.com>
2021-02-17 23:00:38 +0900

    list remove not in

--- list.py
+++ list.py
@@ -1,19 +1,16 @@
 # List
 a = [i for i in range(20) if i % 2 == 1]
 # result : [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-print(a)
 # same result
 a = []
 for i in range(20):
     if i % 2 == 1:
         a.append(i)
-print(a)
 # Two dimension list
 n = 4
 m = 3
 array = [[0] * m for _ in range(n)]
 # result : [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
-print(array)
 # List's functions
 # result : [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
 a.append(21)
@@ -27,3 +24,7 @@ a.insert(1, 21)
 a.count(21)
 # result : [21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1]
 a.remove(21)
+
+remove_set = {3, 5, 7, 9, 11}
+# [21, 19, 17, 15, 13, 1]
+result = [i for i in a if i not in remove_set]

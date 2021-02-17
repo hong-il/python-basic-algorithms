8c30727c47086f9df4bb1556555a73ed901f7d60
KIM HONGIL <clroyan01@gmail.com>
2021-02-18 00:38:12 +0900

    dict - keys list & values list

---
+++ dict.py
@@ -0,0 +1,8 @@
+# dict
+fruit = dict()
+fruit["apple"] = "사과"
+fruit["banana"] = "바나나"
+# dict_keys(['apple', 'banana'])
+key_list = fruit.keys()
+# dict_values(['사과', '바나나'])
+value_list = fruit.values()

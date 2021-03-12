67a17c5c48215d7d81d4f8166cab28368fd5024f
KIM HONGIL <clroyan01@gmail.com>
2021-03-13 00:25:41 +0900

    Heap sort - sort

--- heapSort.py
+++ heapSort.py
@@ -9,3 +9,22 @@ def heapify(li, idx, n):
     if s_idx != idx:
         li[idx], li[s_idx] = li[s_idx], li[idx]
         return heapify(li, s_idx, n)
+
+
+def heap_sort(v):
+    n = len(v)
+    v = [0] + v
+
+    # min heap 생성
+    for i in range(n, 0, -1):
+        heapify(v, i, n)
+
+    # min element extract & heap
+    for i in range(n, 0, -1):
+        print(v[1])
+        v[i], v[1] = v[1], v[i]
+        heapify(v, 1, i - 1)
+
+
+if __name__ == '__main__':
+    heap_sort([5, 3, 4, 2, 1])

f5b85bbcae8cd1cecea15846a341cf964c47180e
KIM HONGIL <clroyan01@gmail.com>
2021-03-25 23:29:09 +0900

    Coord

---
+++ Coord.py
@@ -0,0 +1,11 @@
+class Coord(object):
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def __str__(self):
+        return '({}, {})'.format(self.x, self.y)
+
+
+point = Coord(1, 2)
+# (1, 2)
+print(point)
